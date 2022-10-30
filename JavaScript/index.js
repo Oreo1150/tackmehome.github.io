2ba19b2577c159e0fe2fda74ea7a5c9c96248da1
Oreo1150 <sirachat2046@gmail.com>
2022-10-30 21:57:06 +0700

    code master

---
+++ JavaScript/index.js
@@ -0,0 +1,28 @@
+function linkPage(index) {
+    console.log(index);
+    var link = [
+        "https://www.brandage.com/article/6662/100-PIPERs",
+        "http://teesurablend.blogspot.com/2018/08/blend-285_26.html",
+        "https://rcc-net.org/ballantines-blended-scotch-whisky/",
+        "https://www.johnniewalker.com/th-th/our-whisky/johnnie-walker-colours/%E0%B8%88%E0%B8%AD%E0%B8%AB-%E0%B8%99%E0%B8%99-%E0%B8%A7%E0%B8%AD%E0%B8%A5-%E0%B8%81%E0%B9%80%E0%B8%81%E0%B8%AD%E0%B8%A3-%E0%B8%9A%E0%B8%A5-%E0%B9%80%E0%B8%A5%E0%B9%80%E0%B8%9A-%E0%B8%A5/",
+        "https://th.liq9.asia/blog/jack-daniels-story.html",
+        "https://drunk-man.com/2021/03/johnnie-walker-black-label-%E0%B8%95%E0%B8%AD%E0%B8%99%E0%B8%99%E0%B8%B5%E0%B9%89%E0%B9%80%E0%B8%9B%E0%B9%87%E0%B8%99%E0%B8%A2%E0%B8%B1%E0%B8%87%E0%B9%84%E0%B8%87%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%87/",
+        "https://www.xn--12c1c3aija2as7bqj9k1e.com/johnnie-walker-red-label/",
+        "https://climbers-uh.net/%E0%B9%80%E0%B8%AB%E0%B8%A5%E0%B9%89%E0%B8%B2%E0%B8%AB%E0%B8%87%E0%B8%AA%E0%B9%8C%E0%B8%97%E0%B8%AD%E0%B8%87-17-12-64/thai-alcohol-13-12-64/",
+        "https://th.wikipedia.org/wiki/%E0%B9%81%E0%B8%AA%E0%B8%87%E0%B9%82%E0%B8%AA%E0%B8%A1_(%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B2)",
+        "https://th.liq9.asia/blog/hennessy-cognac-story.html",
+        "https://climbers-uh.net/%E0%B9%80%E0%B8%A1%E0%B8%AD%E0%B8%A3%E0%B8%B4%E0%B9%80%E0%B8%94%E0%B8%B5%E0%B8%A2%E0%B8%99-22-12-64-thai-alcohol-13-12-64/thai-alcohol-13-12-64/",
+        "https://www.xn--12c1c3aija2as7bqj9k1e.com/jim-beam-bourbon/",
+    ];
+
+    window.location.assign(link[index]);
+}
+
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
